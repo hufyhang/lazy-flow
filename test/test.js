b65ee0fbe8bfd0eb78c8dc346d60f1356b43e938
Feifei Hang <feifeihang@travelsky.com>
2018-02-01 02:58:41 +0000

    more tests

--- test/test.js
+++ test/test.js
@@ -1,23 +1,48 @@
 const lazyFlow = require('../dist/lazy').default;
 const assert = require('assert');
 
+const equal = assert.deepEqual;
+
 describe('Lazy Flow', () => {
   it('含有初始值时，应该可以通过value直接拿到初始数组', () => {
-    assert.deepEqual(lazyFlow([1, 2, 3]).value(), [1, 2, 3]);
-    assert.deepEqual(lazyFlow(100).value(), [100]);
+    equal(lazyFlow([1, 2, 3]).value(), [1, 2, 3]);
+    equal(lazyFlow(100).value(), [100]);
   });
 
   describe('#range', () => {
     it('生成一段数组区间', () => {
       const arr = lazyFlow().range(1, 10).value();
-      assert.deepEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      equal(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
   });
 
   describe('#map', () => {
     it('应当完成对数组的映射', () => {
-      const arr = lazyFlow([1, 2, 3]).map(x => x * 10).value();
-      assert.deepEqual(arr, [10, 20, 30]);
+      let arr = lazyFlow([1, 2, 3]).map(x => x * 10).value();
+      equal(arr, [10, 20, 30]);
+
+      arr = lazyFlow().range(1, 5).map(x => x * 100).value();
+      equal(arr, [100, 200, 300, 400, 500]);
+    });
+  });
+
+  describe('#filter', () => {
+    it('应当根据条件函数进行过滤', () => {
+      let arr = lazyFlow([1, 2, 3, 4, 5, 6]).filter(x => x >= 3).value();
+      equal(arr, [3, 4, 5, 6]);
+
+      arr = lazyFlow().range(1, 10).filter(x => x % 2 === 0).map(x => x * 10).value();
+      equal(arr, [20, 40, 60, 80, 100]);
+    });
+  });
+
+  describe('#take', () => {
+    it('应当从数组中取出相应个数的元素', () => {
+      let arr = lazyFlow().range(1, 10).map(x => x).take(5).value();
+      equal(arr, [1, 2, 3, 4, 5]);
+
+      arr = lazyFlow().range(1, 10).filter(x => x % 2 === 0).map(x => x * 10).take(3).value();
+      equal(arr, [20, 40, 60]);
     });
   });
 });
