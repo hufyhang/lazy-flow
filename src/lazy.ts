85fd5be8ca6939b551881fd568e2f4198ae0d16f
Feifei Hang <feifeihang@travelsky.com>
2018-02-01 02:22:19 +0000

    add docs

--- src/lazy.ts
+++ src/lazy.ts
@@ -42,42 +42,72 @@ class Lazy {
     }
   }
 
+  /**
+   * 生成一个范围在[x, y]闭区间的数组。
+   * @param x 起始值
+   * @param y 终止值
+   */
   range(x: number, y: number) {
     const instance = new LazyRange(x, y);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 从对象中取出键名"key"对应的键值，并生成新的数组。
+   * @param key 键名
+   */
   pluck(key: string) {
     const instance = new LazyPluck(key);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 对数组进行迭代计算，最终返回计算后的最终结果。
+   * @param func 用于迭代计算的函数。
+   * @param initialValue 迭代结果的初始值。
+   */
   reduce(func: TReduceFunc, initialValue: any) {
     const instance = new LazyReduce(func, initialValue);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 对数组中的每一项进行映射转换。
+   * @param trans 转换函数。
+   */
   map(trans: TMapTransformer) {
     const instance = new LazyMap(trans);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 对数组中的值进行条件过滤。
+   * @param condition 条件函数。
+   */
   filter(condition: TFilterCondition) {
     const instance = new LazyFilter(condition);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 从数组中取出一定数量的值。（从左侧开始）
+   * @param boundry 所需取出的数量。
+   */
   take(boundry: number) {
     const instance = new LazyTake(this.process, boundry);
     this.pushProcess(instance);
     return this;
   }
 
+  /**
+   * 向Lazy Flow传入新的值并然后流结果。
+   * @param value 新的数组或值。
+   */
   next(value: any|any[]) {
     let val = value;
     if (!isArray(val)) {
@@ -88,6 +118,12 @@ class Lazy {
     return this.value();
   }
 
+  /**
+   * 使Lazy Flow开始执行。
+   * @param processSequence 所需执行的序列
+   * @param boundry 该序列的boundry值，用于对计算进行最小化处理
+   * @param result 当前序列的执行结果
+   */
   value(processSequence: any[] = this.process, boundry: number|null = null, result: any = this._result) {
     for (let process of processSequence) {
       if (process instanceof BoundryObject) {
